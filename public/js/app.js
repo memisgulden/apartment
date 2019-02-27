c2b01dfc1b7eac86b7c8a97a222ff1aa2177beaa
memisgulden <memis@gmail.com>
2019-02-27 16:12:25 -0500

    navbar styling

--- public/js/app.js
+++ public/js/app.js
@@ -4,10 +4,8 @@ $(document).ready(function () {
     $('.carousel').carousel({
         interval: 3000
     }),
+    
     $('.sidenav').sidenav();
-
-    $('a').hover('background-color: pink');
-
 
 
 });
