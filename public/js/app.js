2002379a83d2475890e8632620ef66dde96f0a8c
memisgulden <memis@gmail.com>
2019-02-27 15:35:04 -0500

    server

---
+++ public/js/app.js
@@ -0,0 +1,14 @@
+console.log("I work!");
+
+$(document).ready(function () {
+    $('.carousel').carousel({
+        interval: 3000
+    }),
+    $('.sidenav').sidenav();
+
+    $('a').hover('background-color: pink');
+
+
+
+});
+
